165e01d725c96f6b2539d6945d65c0717237e5b2
RT1811 <ritwick.thebest@gmail.com>
2026-08-24 04:47:09 -0700

    Add Fetch pokemon cards from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
+import { useState, useEffect } from "react";
 import Scoreboard from "./components/Scoreboard.jsx";
 import CardGrid from "./components/CardGrid.jsx";
 
-const placeholderCards = [
-  { id: 1, name: "Pikachu", image: "https://via.placeholder.com/150" },
-  { id: 2, name: "Bulbasaur", image: "https://via.placeholder.com/150" },
-  { id: 3, name: "Charmander", image: "https://via.placeholder.com/150" },
-  { id: 4, name: "Squirtle", image: "https://via.placeholder.com/150" },
-];
-
 function App() {
+  const [cards,setCards] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      const randomIDs = new Set();
+      while (randomIDs.size < 12) {
+        randomIDs.add(Math.floor(Math.random() * 151) + 1);
+      }
+
+      const requests = [...randomIDs].map((id) =>
+      fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => res.json())
+    );
+
+    const result = await Promise.all(requests);
+
+    const formattedCards = result.map((pokemon) => ({
+      id: pokemon.id,
+      name: pokemon.name,
+      image: pokemon.sprites.front_default,
+    }))
+
+    setCards(formattedCards);
+    }
+
+    fetchPokemon();
+  }, [])
+
   return (
     <div className="app">
       <h1>Memory Card Game</h1>
       <Scoreboard score={0} bestScore={0} />
-      <CardGrid cards={placeholderCards} />
+      <CardGrid cards={cards} />
     </div>
   );
 }
